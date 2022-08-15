e64e772f0ffc1766cccafe53de52241cae774306
Giuseppe Barillari <giuseppe.barillari@sky.uk>
2022-08-15 10:25:47 +0100

    Spectacle setup
    Signed-off-by: Giuseppe Barillari <giuseppe.barillari@sky.uk>

---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    primary: "red",
+    secondary: "green",
+  },
+  fonts: {
+    header: '"Roboto", Helvetica, Arial, sans-serif',
+  },
+  fontSizes: {
+    h1: "72px",
+    h2: "64px",
+  },
+};
